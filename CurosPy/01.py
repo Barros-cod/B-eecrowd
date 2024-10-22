bb1d12df31dd2d1b1b50ca3e9f5181eee3156993
Barros <itzbarros2017@gmail.com>
2024-10-21 22:05:51 -0300

    Update

---
+++ CurosPy/01.py
@@ -0,0 +1,34 @@
+# if / elif  / else
+# se / se não se /se nao
+
+# entrada = input('Voce quer "s" ou "n" ')
+
+# if entrada == 's':
+# 	print('Voce entrou no sistema')
+# elif entrada == 'n':
+# 	print('Voce saiu do sistema')
+# else:
+# 	print('Fora..')
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+
+if condicao1:
+	print('Este é o código do if')
+	#Toma cuidado com a indentação!
+elif condicao2:
+	print('Codigo2') 
+elif condicao3:
+	print('Codigo3') 
+elif condicao4:
+	print('Codigo4') 
+else:
+	print('Acabou o lol')
+
+if 10 == 10:
+	print('Outro if')
+
+print('Fora do if')
